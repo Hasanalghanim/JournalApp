0e1325d5fbadca16fab0933ef7590d8999028df1
hasan <hasanalghanim@gmail.com>
2020-12-18 22:28:54 -0700

    Auth system  in place

--- mybook/main/views.py
+++ mybook/main/views.py
@@ -33,27 +33,28 @@ def loginuser(request):
     else:
         username = request.POST['username']
         password = request.POST['password']
-        user = authenticate(request, username=username, password=password,)
+        user = authenticate(request, username=username, password=password)
 
-        if user is None:
-            return render(request, 'main/loginuser.html', {'form': AuthenticationForm(), 'error': 'Username and password did not match. '})
+        if user is not None:
+                login(request, user)
+                return redirect("mainlogin")
+
         else:
-            login(request, user)
-            return redirect("mainlogin")
+            return render(request, 'main/loginuser.html', {'form': AuthenticationForm(), 'error': 'Username and password did not match. '})
 
-
+@login_required
 def logoutuser(request):
     if request.method == 'POST':
         logout(request)
-    return render(request, 'main/home.html', {'logout': "youve logged out now"})
+    return redirect('home')
 
 
-
+@login_required
 def mainlogin(request):
     entries = Entry.objects.filter(user=request.user)
     return render(request, 'main/mainlogin.html', {"entries": entries })
 
-
+@login_required
 def makeentry(request):
     if request.method == 'GET':
         return render(request, 'main/makeentry.html', {'form': EntryForm()})
@@ -68,7 +69,7 @@ def makeentry(request):
             return render(request, 'main/makeentry.html', {'form': EntryForm(), 'error': 'check the fields'})
 
 
-
+@login_required
 def viewentry(request, Entry_pk):
     entry = get_object_or_404(Entry, pk=Entry_pk, user=request.user)
     if request.method =="GET":
--- mybook/mybook/settings.py
+++ mybook/mybook/settings.py
@@ -118,5 +118,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+LOGIN_URL = '/loginuser'
+
 
 STATIC_URL = '/static/'

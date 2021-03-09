e7659b8571671ba09e9188ef292a548f8c0924b0
hasan <hasanalghanim@gmail.com>
2021-03-08 22:52:37 -0700

    Css update
    Css update

--- mybook/mybook/urls.py
+++ mybook/mybook/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from main import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -39,4 +41,4 @@ urlpatterns = [
     path('<slug:slug>/edit', views.editentry, name='editentry'),
     path('<slug:slug>/delete', views.deleteentry, name='deleteentry'),
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
